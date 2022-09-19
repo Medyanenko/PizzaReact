ebff18e30a5fafcdc11521b7636127c82656969c
Medyanenko <t.kudryavska@gmail.com>
2022-09-19 13:39:11 +0300

    10.feltering static array on backend, pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,14 @@ import PizzaBlock from "./../components/PizzaBlock/PizzaBlock";
 import Skeleton from "./../components/PizzaBlock/Skeleton";
 import Sort from "./../components/Sort/Sort";
 import Categories from "./../components/Categories/Categories";
+import Pagination from "../components/Pagination/Pagination";
+
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const [sortType, setSortType] = useState({
     name: "популярністю",
     sortProperty: "rating",
@@ -20,8 +23,9 @@ const Home = ({ searchValue }) => {
     const order = sortType.sortProperty.includes("-") ? "desc" : "asc";
     const sortBy = sortType.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
     fetch(
-      `https://6324553abb2321cba929fab0.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://6324553abb2321cba929fab0.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -29,7 +33,7 @@ const Home = ({ searchValue }) => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
 
   const pizzas = items
     .filter((obj) => {
@@ -54,6 +58,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage = {(number)=>setCurrentPage(number) }/>
     </div>
   );
 };
