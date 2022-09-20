c9549583e9f6f6514f3c304407cd0bdf825eb522
Medyanenko <t.kudryavska@gmail.com>
2022-09-20 15:26:03 +0300

    14.using useref for input, using lodash, debouns, usecallback and usememo

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
-import { useContext } from 'react';
-import { SearchContext } from '../../App';
-import s from "./Search.module.scss"
+import React, { useState } from "react";
+import { useContext, useRef, useMemo } from "react";
+import debounce from "lodash.debounce";
+import { SearchContext } from "../../App";
+import s from "./Search.module.scss";
 const Search = () => {
-  const {searchValue, setSearchValue} = useContext(SearchContext);
+  const { setSearchValue } = useContext(SearchContext);
+  const [value, setValue] = useState("");
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setValue("");
+    setSearchValue("");
+    inputRef.current.focus();
+  };
+  const updateSearchValue = useMemo(
+    () =>
+      debounce((str) => {
+        setSearchValue(str);
+      }, 250),
+    [setSearchValue]
+  );
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
   return (
     <div className={s.root}>
-        <img className={s.icon} src="./img/search.svg" alt="search"/>
-        <input value={searchValue} onChange={(event) => setSearchValue(event.target.value)} className={s.input} placeholder='Пошук піци...'/>
-        {searchValue&&<img className={s.clear} onClick = {()=>setSearchValue('')}src="./img/btn-remove.svg" alt="btn-remove"/>}
+      <img className={s.icon} src="./img/search.svg" alt="search" />
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={s.input}
+        placeholder="Пошук піци..."
+      />
+      {value && (
+        <img
+          className={s.clear}
+          onClick={onClickClear}
+          src="./img/btn-remove.svg"
+          alt="btn-remove"
+        />
+      )}
     </div>
- 
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
 import { useContext } from "react";
 
@@ -9,20 +10,23 @@ import Categories from "./../components/Categories/Categories";
 import Pagination from "../components/Pagination/Pagination";
 
 import { SearchContext } from "../App";
-import { setCategoryId } from "../redux/slices/filterSlice";
+import { setCategoryId, setCurrentPage } from "../redux/slices/filterSlice";
 
 const Home = () => {
-  const { categoryId, sort } = useSelector((state) => state.filter);
+  const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
   const { searchValue } = useContext(SearchContext);
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [currentPage, setCurrentPage] = useState(1);
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
   };
+
+  const onChangePage =(number)=>{
+    dispatch(setCurrentPage(number))
+  }
 
   useEffect(() => {
     setIsLoading(true);
@@ -31,12 +35,13 @@ const Home = () => {
     const sortBy = sort.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const search = searchValue ? `&search=${searchValue}` : "";
-    fetch(
-      `https://6324553abb2321cba929fab0.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
-    )
-      .then((res) => res.json())
-      .then((arr) => {
-        setItems(arr);
+
+    axios
+      .get(
+        `https://6324553abb2321cba929fab0.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+      )
+      .then((res) => {
+        setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
@@ -62,7 +67,7 @@ const Home = () => {
       </div>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination onChangePage={(number) => setCurrentPage(number)} />
+      <Pagination onChangePage={onChangePage} />
     </div>
   );
 };
